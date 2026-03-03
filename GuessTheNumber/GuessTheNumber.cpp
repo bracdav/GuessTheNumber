f06cbe39f62337d5b73686c381a29f6a6a066d1b
David Yoga <david.m.bracher@gmail.com>
2026-03-03 10:57:12 +0100

    adding option for hamving multiple games

--- GuessTheNumber/GuessTheNumber.cpp
+++ GuessTheNumber/GuessTheNumber.cpp
@@ -8,28 +8,54 @@
 
 using namespace std;
 
+bool keepPlaying(bool keep_playing) 
+{
+    string play_string;
+    std::cout << "Wanna keep playing? (yes/no)\n";
+    std::cin >> play_string;
+
+    while (play_string != "y" && play_string != "n") 
+    {
+        std::cout << "Allowed inputs are y or n\n";
+        std::cin >> play_string;
+    }
+
+    if (play_string == "n")
+    {
+        return false;
+    }
+
+    return keep_playing;
+}
+
 int main()
 {
-    GuessingConfigs config;
-    string input;
+    bool play = true;
+
+    while (play) {
+        GuessingConfigs config;
+        string input;
 
 
 
-    std::cout << "Lower bound?\n";
-    std::cin >> input;
-    config.lower_bound = NumberOperations::convertToInt(input);
+        std::cout << "Lower bound?\n";
+        std::cin >> input;
+        config.lower_bound = NumberOperations::convertToInt(input);
 
-    std::cout << "Upper bound?\n";
-    std::cin >> input;
-    config.upper_bound = NumberOperations::convertToInt(input);
+        std::cout << "Upper bound?\n";
+        std::cin >> input;
+        config.upper_bound = NumberOperations::convertToInt(input);
 
 
-    config.setNumberToGuess();
+        config.setNumberToGuess();
 
-    std::cout << "Make your first guess:\n";
-    std::cin >> input;
+        std::cout << "Make your first guess:\n";
+        std::cin >> input;
 
-    config.guess = NumberOperations::convertToInt(input);
+        config.guess = NumberOperations::convertToInt(input);
 
-    NumberOperations::guessNumber(config);
+        NumberOperations::guessNumber(config);
+
+        play = keepPlaying(play);
+    }
 }
